88a20b9391fc4f7bf28fc292c3797de248abfb5b
Kraigo <kraigo@live.com>
2016-08-09 09:39:42 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const gulp = require('gulp');
+const react = require('gulp-react');
+const babel = require('gulp-babel');
+
+gulp.task('jsx', () => {
+    return gulp.src('./public/scripts/*.jsx')
+        .pipe(react())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./public/scripts'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./public/scripts/*.jsx', ['jsx']);
+});
+
+gulp.start('watch');
