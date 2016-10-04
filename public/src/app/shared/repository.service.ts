1b0a848a6eafd1ff914d919fed28026401bfc70e
Kraigo <kraigo@live.com>
2016-10-04 13:13:05 +0300

    new build, fixed api

--- public/src/app/shared/repository.service.ts
+++ public/src/app/shared/repository.service.ts
@@ -11,7 +11,7 @@ import { Invitation } from './models/invitation.model';
 @Injectable()
 
 export class RepositoryService {
-    baseUrl: string = 'http://localhost:3000/api';
+    baseUrl: string = '/api';
     tasksUrl: string = this.baseUrl + '/tasks';
     projectsUrl: string = this.baseUrl + '/projects';
     teamsUrl: string = this.baseUrl + '/teams';
